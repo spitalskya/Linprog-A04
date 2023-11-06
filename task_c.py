460ced9e16aa159e2c0672e1274bc25c8e0a86ce
teoPazera <pazerateo@gmail.com>
2023-11-06 18:13:26 +0100

    a bit updated version of task C

--- task_c.py
+++ task_c.py
@@ -9,13 +9,13 @@ x = data[['WinterRain','AGST', 'HarvestRain', 'Age', 'FrancePop']]
 numberOfVariablesBeta = x.shape[1]+1
 
 #l1 norm
-c = np.array([0]*numberOfVariablesBeta + [1]* len(x.values))
-ALeft = np.matrix([[1]*len(x.values)]).transpose()
+c = np.array([0]*numberOfVariablesBeta + [1] * len(x.values))
+ALeft = np.matrix([ [1] * len(x.values)]).transpose()
 ARigth = np.matrix(x.values)
 A = np.block([ALeft, ARigth])
 I = np.identity(len(x.values))
 
-A_ub = np.block([[-A,-I],[A,-I]])
+A_ub = np.block([[-A, -I], [A, -I]])
 b_ub = np.concatenate([-y, y])
 solve = linprog(c, A_ub, b_ub)
 
@@ -25,7 +25,7 @@ print(bethas)
 #l inf norm
 c_inf = np.array([0]*numberOfVariablesBeta + [1])
 A_inf = np.block([ALeft, ARigth])
-i_inf = np.array([[1] * len(x.values)]).transpose()
+i_inf = np.array([ [1] * len(x.values)]).transpose()
 
 A_ub_inf = np.block([[-A_inf,-i_inf],[A_inf,-i_inf]])
 b_ub_inf = np.concatenate([-y, y])
