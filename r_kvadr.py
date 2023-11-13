22a43cb43189470691f5fa32a84947064c13cdbb
unknown <robertkenderes350@gmail.com>
2023-11-13 19:43:40 +0100

    finished R^2 for multple xvectors

--- r_kvadr.py
+++ r_kvadr.py
@@ -3,19 +3,28 @@ import numpy as np
 import pandas as pd
 from Task_E.normModels import L1Model, LInfModel
 
-def r_kvadr(path: str, norm: Union[L1Model, LInfModel]) -> int:
-    data = np.load(path)
-    x = data['x']
-    y = data['y']
-    model = norm(y, np.array([x]))
-    betas = model.solve()
-    y_hat = [i * betas[1] + betas[0] for i in x]
-    y_mean = np.mean(y)
-    res1 = np.sum((np.array(y) - np.array(y_hat)) ** 2)
-    res2 = np.sum((np.sum((np.array(y) - y_mean) ** 2)))
-    result = 1 - (res1 / res2)
-    return result
+class Model:
+    def r_kvadr(self, path: str, norm: Union[L1Model, LInfModel], xcols: list) -> float:
+        data = pd.read_csv('Linprog-A04/'+path)
+        
+        y = np.array(data['Price'])
+        x = data[xcols].to_numpy().transpose()
+        
+        model = norm(y, x)
+        betas = model.solve()
+
+        y_hat = betas[0] + np.dot(x.transpose(), betas[1:])
+        y_mean = np.mean(y)
+
+        res1 = np.sum((y - y_hat) ** 2)
+        res2 = np.sum((y - y_mean) ** 2)
+        result = 1 - (res1 / res2)
+        return result
 
 
-
-print(r_kvadr('data/A04plotregres.npz', L1Model))
+xvectors = ['Year', 'WinterRain', 'AGST', 'HarvestRain', 'Age', 'FrancePop']
+model = Model()
+r2_l1 = model.r_kvadr('data/A04wine.csv', L1Model, xvectors)
+r2_linf = model.r_kvadr('data/A04wine.csv', LInfModel, xvectors)
+print(f'R-squared L1: {r2_l1}')
+print(f'R-squared LInf: {r2_linf}')
