e03fccbab5d37d98e611ce1914594f4d5d21c74c
erikbozik <erik23.bozik@gmail.com>
2023-11-15 10:15:17 +0100

    Basic visuals test.

--- Task_E/visualizations.py
+++ Task_E/visualizations.py
@@ -1,36 +1,68 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 import itertools
-from normModels import Model, L1Model, LInfModel
-from scipy.optimize import linprog
+from normModels import L1Model, LInfModel
+from DataGenerator import DataGenerator
+
+
+def data_2d(start=10, finish=200, size=40, noise=(100, 500)):
+    generator = DataGenerator()
+    return generator.gen_2d_data(start=start, finish=finish, size=size, noise=noise)
+
+
+def data_3d(start=10, finish=200, size=40, noise=(100, 500)):
+    generator = DataGenerator()
+    return generator.gen_3d_data(start=start, finish=finish, size=size, noise=noise)
+
 
 def reg_plot2d(x, y, solved_model, ax):
-    ax.scatter(x, y)
+    # ax.scatter(x, y)
     beta0, beta1 = solved_model.beta
-    ax.set_title(np.corrcoef(x, y))
     reg = [i * beta1 + beta0 for i in x]
     ax.plot(x, reg)
 
+def reg_plot3d(x1, x2, y, solved_model, ax):
+    ax.scatter(x1, x2, y, c='r')
+    beta0, beta1, beta2 = solved_model.beta
+    x1, x2 = np.meshgrid(x1, x2)
+    reg = beta0 + x1 * beta1 + x2 * beta2
+
+    ax.plot_surface(x1, x2, reg, cmap='Blues')
+
+def random_l1_2dplot(ax, data):
+    x, y = data
+    model1 = L1Model(y, np.array([x]))
+    model1.solve()
+    return reg_plot2d(x, y, model1, ax)
+
+
+def random_linf_2dplot(ax, data):
+    x, y = data
+    model1 = LInfModel(y, np.array([x]))
+    model1.solve()
+    return reg_plot2d(x, y, model1, ax)
+
+def random_l1_3dplot(ax, data):
+    x1, x2, y = data
+    model1 = L1Model(y, np.array([x1, x2]))
+    model1.solve()
+    return reg_plot3d(x1, x2, y, model1, ax)
+
+
 if __name__ == '__main__':
-    data = pd.read_csv('..\data\A04wine.csv')
-    data_combinations = list(itertools.combinations(data.columns, 2))
-    fig, axes = plt.subplots(len(data_combinations), 2, figsize=(20, 80))
+    # fig, axes = plt.subplots(2, sharex=True, sharey=True)
+    # for i in range(2):
+    #     data = data_2d()
+    #     random_l1_2dplot(axes[i], data)
+    #     random_linf_2dplot(axes[i], data)
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    data = data_3d()
+    random_l1_3dplot(ax, data)
+    plt.show()
 
-    for row, combination in enumerate(data_combinations):
-        x_axis, y_axis = combination
-        x = np.array(data[x_axis])
-        y = np.array(data[y_axis])
-
-        model1 = L1Model(y, np.array([x]))
-        model1.solve()
-        reg_plot2d(x, y, model1, axes[row, 0])
-
-        model2 = LInfModel(y, np.array([x]))
-        model2.solve()
-        reg_plot2d(x, y, model2, axes[row, 1])
-
-    plt.savefig('compare_models.jpg')
 
 
 
