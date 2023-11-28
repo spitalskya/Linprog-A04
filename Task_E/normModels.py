6ef949e66daed9d32f4a2c1fb596dac61065eb75
unknown <robertkenderes350@gmail.com>
2023-11-28 17:17:42 +0100

    r_kvadr supnuta dovnutra model classy

--- Task_E/normModels.py
+++ Task_E/normModels.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+from typing import Union
 import numpy as np
 from scipy.optimize import linprog, OptimizeResult
 from typing import List
@@ -28,6 +29,25 @@ class Model:
         self.solved = None
         # beta values
         self.beta = None
+
+    def r_kvadr(self, norm: Union[L1Model, LInfModel]) -> float:        
+        model = norm(self.y, self.x_vect)
+        betas = model.solve()
+
+        y_hat = betas[0] + np.dot(self.x_vect.transpose(), betas[1:])
+        y_mean = np.mean(self.y)
+
+        # print(self.y - y_hat)
+
+        res1 = 0
+        res2 = 0
+
+        for i in range(len(self.y)):
+            res1 += (self.y[i] - y_hat[i]) ** 2
+            res2 += (self.y[i] - y_mean) ** 2
+
+        result = 1 - (res1 / res2)
+        return result
 
 
 class L1Model(Model):
--- r_kvadr_universal.py
+++ r_kvadr_universal.py
@@ -1,50 +1,46 @@
 from typing import Union
 import numpy as np
 import pandas as pd
-from Task_E.normModels import L1Model, LInfModel
+from Task_E.normModels import L1Model, LInfModel, Model
 
-class Model:
-    def r_kvadr(self, x, y, norm: Union[L1Model, LInfModel]) -> float:        
-        model = norm(y, x)
-        betas = model.solve()
+# class Model:
+#     def r_kvadr(self, x, y, norm: Union[L1Model, LInfModel]) -> float:        
+#         model = norm(y, x)
+#         betas = model.solve()
         
-        # x su riadky
-        # y je stlpec
+#         # x su riadky
+#         # y je stlpec
 
-        y_hat = betas[0] + np.dot(x.transpose(), betas[1:])
-        y_mean = np.mean(y)
+#         y_hat = betas[0] + np.dot(x.transpose(), betas[1:])
+#         y_mean = np.mean(y)
 
-        print(y - y_hat)
+#         print(y - y_hat)
 
-        res1 = 0
-        res2 = 0
+#         res1 = 0
+#         res2 = 0
 
-        for i in range(len(y)):
-            res1 += (y[i] - y_hat[i]) ** 2
-            res2 += (y[i] - y_mean) ** 2
+#         for i in range(len(y)):
+#             res1 += (y[i] - y_hat[i]) ** 2
+#             res2 += (y[i] - y_mean) ** 2
 
-        '''res1 = np.sum((y - y_hat) ** 2)
-        res2 = np.sum((y - y_mean) ** 2)'''
-        result = 1 - (res1 / res2)
-        return result
+#         '''res1 = np.sum((y - y_hat) ** 2)
+#         res2 = np.sum((y - y_mean) ** 2)'''
+#         result = 1 - (res1 / res2)
+#         return result
 
-'''
-xvectors = ['Year', 'WinterRain', 'AGST', 'HarvestRain', 'Age', 'FrancePop']
-model = Model()
-r2_l1 = model.r_kvadr('data/A04wine.csv', L1Model, xvectors)
-r2_linf = model.r_kvadr('data/A04wine.csv', LInfModel, xvectors)
-print(f'R-squared L1: {r2_l1}')
-print(f'R-squared LInf: {r2_linf}')
-'''
+# wine  = pd.read_csv('data/A04wine.csv')
+# x_names = ['Year', 'WinterRain', 'AGST', 'HarvestRain', 'Age', 'FrancePop']
+# x = wine[x_names].to_numpy().transpose()
+# y = wine['Price']
 
 diabetes = pd.read_csv('data/diabetes.csv')
 x_names = ['Pregnancies','BloodPressure','SkinThickness','Insulin','BMI','DiabetesPedigreeFunction','Age']
 x = diabetes[x_names].to_numpy().transpose()
 y = diabetes['Glucose']
-model = Model()
+model = Model(y, x)
 
-r2_l1 = model.r_kvadr(x, y, L1Model)
-r2_linf = model.r_kvadr(x, y, LInfModel)
+r2_l1 = model.r_kvadr(L1Model)
+r2_linf = model.r_kvadr(LInfModel)
 
 print(f'R-squared L1: {r2_l1}')
 print(f'R-squared LInf: {r2_linf}')
