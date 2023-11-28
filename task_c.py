3a3abba43a3bdfcd4f4fd183b9c1b7bff064ab21
teoPazera <149321729+teoPazera@users.noreply.github.com>
2023-11-28 15:59:04 +0100

    Update task_c.py
    commented and renamed a few variables to look more presentable

--- task_c.py
+++ task_c.py
@@ -1,35 +1,53 @@
 import numpy as np
 from scipy.optimize import linprog
 import pandas as pd
-
+#importing data and creating dataframes
 data = pd.read_csv('data\A04wine.csv')
 
+# Separate dependent variable (y) and independent variables (x)
 y = data['Price']
 x = data[['WinterRain','AGST', 'HarvestRain', 'Age', 'FrancePop']]
-numberOfVariablesBeta = x.shape[1]+1
+# Calculate the number of variables (features) plus 1 for the intercept term
+numberOfVariablesBeta = x.shape[1] + 1
 
-#l1 norm
-c = np.array([0]*numberOfVariablesBeta + [1] * len(x.values))
-ALeft = np.matrix([ [1] * len(x.values)]).transpose()
-ARigth = np.matrix(x.values)
-A = np.block([ALeft, ARigth])
-I = np.identity(len(x.values))
+#---------------------------------------------------------------------------------------------------------------------------------------
 
+# Formulating the linear programming problem for l1 norm (minimizing the sum of absolute values of coefficients)
+c = np.array([0]*numberOfVariablesBeta + [1] * len(x.values)) # Objective function coefficients
+
+ALeft = np.matrix([ [1] * len(x.values)]).transpose() # Coefficients for beta0
+ARigth = np.matrix(x.values) # Coefficients for other independent variables beta
+A = np.block([ALeft, ARigth]) # Concenate coefficients of variables into one matrix
+
+I = np.identity(len(x.values))# Identity matrix for intercept temr
+
+# Formulate inequality constraints for l1 norm
 A_ub = np.block([[-A, -I], [A, -I]])
 b_ub = np.concatenate([-y, y])
-solve = linprog(c, A_ub, b_ub)
 
-bethas = solve.x[:numberOfVariablesBeta]
-print(bethas)
+# Solve the linear programming problem for l1 norm
+solve = linprog(c, A_ub, b_ub, bounds = [(None,None)]*numberOfVariablesBeta +[(0, None)] * len(x.values))
 
-#l inf norm
-c_inf = np.array([0]*numberOfVariablesBeta + [1])
-A_inf = np.block([ALeft, ARigth])
-i_inf = np.array([ [1] * len(x.values)]).transpose()
+#extract variables(betas) and print them out
+betas = solve.x[:numberOfVariablesBeta]
+print(betas)
 
+#---------------------------------------------------------------------------------------------------------------------------------------
+
+# Formulating the linear programming problem for l-inf norm (minimizing the maximum of absolute values of coefficients)
+c_inf = np.array([0]*numberOfVariablesBeta + [1]) # Objective function coefficients
+
+A_inf = np.block([ALeft, ARigth]) # Coefficients for independent variables for l∞ norm
+
+i_inf = np.array([ [1] * len(x.values)]).transpose() # Coefficients for the intercept term for l∞ norm
+
+# Formulate inequality constraints for l∞ norm
 A_ub_inf = np.block([[-A_inf,-i_inf],[A_inf,-i_inf]])
-b_ub_inf = np.concatenate([-y, y])
-solve_inf = linprog(c_inf, A_ub_inf, b_ub_inf)
+b_ub_inf = np.concatenate([-y, y]) 
 
-bethas_inf = solve_inf.x[:numberOfVariablesBeta]
-print(bethas_inf)
+# Solve the linear programming problem for l∞ norm
+solve_inf = linprog(c_inf, A_ub_inf, b_ub_inf, bounds=[(None,None)]*numberOfVariablesBeta+[(0,None)])
+
+#extract betas and print them out
+betas_inf = solve_inf.x[:numberOfVariablesBeta]
+print(betas_inf)
